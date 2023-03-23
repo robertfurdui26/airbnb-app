9f8a42e86331dbba47292dd077b0b8dd3aaca854
robertfurdui26 <125736128+robertfurdui26@users.noreply.github.com>
2023-03-23 21:56:27 +0200

    comiit now

---
+++ src/Components/Navbar.js
@@ -0,0 +1,12 @@
+import React from "react";
+import logo from "./airbnb.jpg";
+
+function Navbar() {
+  return (
+    <nav>
+      <img src="{logo}}" alt="logo" />
+    </nav>
+  );
+}
+
+export default Navbar;
